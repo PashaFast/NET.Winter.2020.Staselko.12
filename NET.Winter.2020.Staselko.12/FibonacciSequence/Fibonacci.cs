0b5b1ccdf237581a403283cf3056012461c88d69
PashaFast <pashahit@gmail.com>
2020-02-13 23:07:24 +0300

    feat: Add Fibonacci class with method FibonacciGenerator

--- NET.Winter.2020.Staselko.12/FibonacciSequence/Fibonacci.cs
+++ NET.Winter.2020.Staselko.12/FibonacciSequence/Fibonacci.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace FibonacciSequence
 {
-    public class Fibonacci
+    public static class Fibonacci
     {
+        public static IEnumerable<BigInteger> FibonacciGenerator(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("cannot be zero or negative.");
+            }
+
+            BigInteger current = -1;
+            BigInteger next = 1;
+            for (int i = 0; i < count; i++)
+            {
+                BigInteger temp = next;
+                next += current;
+                current = temp;
+                yield return next;
+            }
+        }
     }
 }
